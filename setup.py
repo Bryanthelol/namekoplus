548817ea1ba78fc2af23cd263ff494c4aabccd0c
Bryant He <bryantsisu@qq.com>
2023-10-24 22:32:04 +0800

    doc: update readme (#34)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='namekoplus',
-    version='0.4.25',
+    version='0.4.26',
     description='A lightweight Python distributed microservice solution',
     long_description=long_description,
     long_description_content_type='text/markdown',
